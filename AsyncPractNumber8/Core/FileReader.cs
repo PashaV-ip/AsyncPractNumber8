ba9dcbb2ba412a2d4571d98788231c5daccd7b3f
Vanshev <pav-alva@ya.ru>
2022-11-14 22:00:50 +0300

    Working

---
+++ AsyncPractNumber8/Core/FileReader.cs
@@ -0,0 +1,40 @@
+﻿using AsyncPractNumber8.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncPractNumber8.Core
+{
+    public static class FileReader
+    {
+        public async static Task<ObservableCollection<Teacher>> FileReadTeacher()
+        {
+            var teacherList = new ObservableCollection<Teacher>();
+            using (StreamReader streamReader = new StreamReader(@"..\..\Files\Teachers.txt"))
+            {
+                string asyncTeacherText = await streamReader.ReadToEndAsync();
+                foreach (var item in asyncTeacherText.Split('\n'))
+                {
+                    var arrayString = item.Split(',');
+                    if (arrayString[0] != "ID")
+                    {
+                        Teacher teacher = new Teacher()
+                        {
+                            ID = int.Parse(arrayString[0]),
+                            FirstName = arrayString[1],
+                            LastName = arrayString[2],
+                            Login = arrayString[3],
+                            Password = arrayString[4]
+                        };
+                        teacherList.Add(teacher);
+                    }
+                }
+            }
+            return teacherList;
+        }
+    }
+}
---
+++ AsyncPractNumber8/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,56 @@
+﻿using AsyncPractNumber8.Command;
+using AsyncPractNumber8.Core;
+using AsyncPractNumber8.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace AsyncPractNumber8.ViewModel
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private ObservableCollection<Teacher> _teachersList;
+        private Teacher _teacher;
+        public Teacher Teacher
+        {
+            get => _teacher;
+            set
+            {
+                _teacher = value;
+                OnPropertyChanged(nameof(Teacher));
+            }
+        }
+        public ObservableCollection<Teacher> TeacherList
+        {
+            get => _teachersList;
+            set
+            {
+                _teachersList = value;
+                OnPropertyChanged(nameof(TeacherList));
+            }
+        }
+        public async void ReadFile()
+        {
+            TeacherList = await FileReader.FileReadTeacher();
+        }
+        public void EqualsLINQ(object Object)
+        {
+            if (_teachersList.FirstOrDefault(x => x.FirstName == _teacher.FirstName && x.Login == _teacher.Login) != null)
+            {
+                MessageBox.Show("Такой пользователь есть!", "Ура..", MessageBoxButton.OK);
+            }
+            else MessageBox.Show("Такого пользователя нету!", "Проблема..", MessageBoxButton.OK);
+        }
+        public ICommand EqualsLINQCommand { get; }
+        public MainWindowViewModel()
+        {
+            _teacher = new Teacher();
+            EqualsLINQCommand = new RelativCommand(EqualsLINQ);
+        }
+    }
+}
